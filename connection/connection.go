10071f2ec56fc30d2202db6aefbd1cdbcb6336bc
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-16 22:06:50 +0700

    update: env

--- connection/connection.go
+++ connection/connection.go
@@ -3,6 +3,8 @@ package connection
 import (
 	"fly/user"
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -10,19 +12,40 @@ import (
 )
 
 func NewConnection(envPath string) (*gorm.DB, error) {
-	env, err := godotenv.Read(envPath)
-	if err != nil {
-		return &gorm.DB{}, err
+	env, _ := godotenv.Read(envPath)
+
+	username := env["DATABASE_USERNAME"]
+	password := env["DATABASE_PASSWORD"]
+	host := env["DATABASE_HOST"]
+	port := env["DATABASE_PORT"]
+	databaseName := env["DATABASE_NAME"]
+
+	if username == "" {
+		os.Getenv("DATABASE_USERNAME")
+	}
+	if password == "" {
+		os.Getenv("DATABASE_PASSWORD")
+	}
+	if host == "" {
+		os.Getenv("DATABASE_HOST")
+	}
+	if port == "" {
+		os.Getenv("DATABASE_PORT")
+	}
+	if databaseName == "" {
+		os.Getenv("DATABASE_NAME")
 	}
 
 	dsn := fmt.Sprintf(
 		"%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
-		env["DATABASE_USERNAME"],
-		env["DATABASE_PASSWORD"],
-		env["DATABASE_HOST"],
-		env["DATABASE_PORT"],
-		env["DATABASE_NAME"],
+		username,
+		password,
+		host,
+		port,
+		databaseName,
 	)
+
+	log.Println(dsn)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
