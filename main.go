059ea88f9bac09b66f4c81c8a66737e9d50acc2b
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-16 20:29:42 +0700

    add: gin

--- main.go
+++ main.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("endpoint / hit with status 200OK")
-		response := struct {
-			Status  string `json:"status"`
-			Code    int    `json:"code"`
-			Message string `json:"message"`
-		}{
-			Status:  "success",
-			Code:    http.StatusOK,
-			Message: "hello world",
-		}
-
-		res, err := json.Marshal(response)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		w.Write(res)
-	})
+	router := gin.Default()
+	router.GET("/", home)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -35,9 +19,15 @@ func main() {
 	}
 
 	domain := fmt.Sprintf(":%v", port)
-
-	log.Println("server running on", domain)
-	if err := http.ListenAndServe(domain, nil); err != nil {
+	if err := router.Run(domain); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func home(c *gin.Context) {
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"status":  "success",
+		"code":    http.StatusOK,
+		"message": "hello world",
+	})
 }
