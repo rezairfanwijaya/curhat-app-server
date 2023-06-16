26af3307fb86e21e29a5403a3fdaa83fd14add26
Reza Irfan Wijaya <rezaaagit@gmail.com>
2023-06-16 20:46:44 +0700

    add: user path

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/", home)
+	router.GET("/users", users)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -30,4 +31,33 @@ func home(c *gin.Context) {
 		"code":    http.StatusOK,
 		"message": "hello world",
 	})
+}
+
+func users(c *gin.Context) {
+	user := []struct {
+		ID    int    `json:"id"`
+		Email string `json:"email"`
+		Role  string `json:"role"`
+	}{
+		{
+			ID:    1,
+			Email: "user1@gmail.com",
+			Role:  "user",
+		}, {
+			ID:    2,
+			Email: "user2@gmail.com",
+			Role:  "user",
+		}, {
+			ID:    3,
+			Email: "admin@admin.com",
+			Role:  "admin",
+		},
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"status": "success",
+		"code":   http.StatusOK,
+		"data":   user,
+	})
+
 }
